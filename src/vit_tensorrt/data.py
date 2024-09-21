e24bec1739be3aa3f724ba35dd629e0050903cb3
KyleNewman97 <kyle@thenewmans.com.au>
2024-09-21 12:10:06 +0000

    Feature: Working image transforms.

--- src/vit_tensorrt/data.py
+++ src/vit_tensorrt/data.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
 import torch
+from PIL import Image
 from torch import Tensor
 from torch.utils.data import Dataset
-from torchvision.transforms import Resize
+from torchvision.transforms import v2
+from torchvision.transforms.functional import pil_to_tensor
+
+from vit_tensorrt.utils import MetaLogger
 
 
 class Letterbox:
@@ -39,7 +43,7 @@ class Letterbox:
         # Resize the image to the correct scale
         new_height = int(scale * height)
         new_width = int(scale * width)
-        resizer = Resize((new_height, new_width))
+        resizer = v2.Resize((new_height, new_width))
         image = resizer(image)
 
         # Overlay on the output image size leaving padding
@@ -59,21 +63,142 @@ class Letterbox:
         )
 
 
-class ViTDataset(Dataset):
-    def __init__(self, data_path: Path, labels, transform=None):
-        self.data_path = data_path
-        self.image_files = list(data_path.glob("*.jpg")) + list(data_path.glob("*.png"))
-        self.labels = labels
-        self.transform = transform
+class OptionalDivision:
+    """
+    Divides the image's content by `255` if the max value in the image is greater than
+    1.
+    """
 
-    # def __len__(self):
-    #     return len(self.data)
+    def __call__(self, image: Tensor) -> Tensor:
+        if 1 < image.max():
+            return image / 255
+        return image
 
-    # def __getitem__(self, idx):
-    #     sample = self.data[idx]
-    #     label = self.labels[idx]
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
 
-    #     if self.transform:
-    #         sample = self.transform(sample)
 
-    #     return sample, label
+class ViTImageTransform(v2.Compose):
+    def __init__(self, height: int = 768, width: int = 768):
+        v2.Compose.__init__(
+            self,
+            [
+                Letterbox(height, width),
+                v2.RandomHorizontalFlip(p=0.5),
+                v2.ToDtype(torch.float32, True),
+                OptionalDivision(),
+                v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ],
+        )
+
+
+class ViTDataset(Dataset, MetaLogger):
+    def __init__(
+        self,
+        images_path: Path,
+        labels_path: Path,
+        num_classes: int,
+        image_height: int = 768,
+        image_width: int = 768,
+    ):
+        """
+        Parameters
+        ----------
+        images_path:
+            Path to a subset (train, valid, test) of the image data.
+
+        labels_path:
+            Path to a subset (train, valid, test) of the label data.
+
+        num_classes:
+            The number of classes classification will be run on.
+
+        image_height:
+            The height of the image to pass to the network, in pixels.
+
+        image_width:
+            The width of the image to pass to the network, in pixels.
+        """
+        MetaLogger.__init__(self)
+        self.transform = ViTImageTransform(image_height, image_width)
+
+        self.images_path = images_path
+        self.labels_path = labels_path
+        self.num_classes = num_classes
+        self.image_height = image_height
+        self.image_width = image_width
+
+        images = list(images_path.glob("*.jpg")) + list(images_path.glob("*.png"))
+        labels = list(labels_path.glob("*.txt"))
+
+        self.logger.info(f"Found {len(images)} images and {len(labels)} labels.")
+
+        self.samples = self._get_file_pairs(images, labels)
+        self.samples = self._filter_for_valid_labels(self.samples)
+
+        self.logger.info(f"{len(self.samples)} valid image and label pairs exist.")
+
+    def _get_file_pairs(
+        self, images: list[Path], labels: list[Path]
+    ) -> list[tuple[Path, Path]]:
+        """
+        Keeps only image and label files that form a pair. A pair requires the image
+        and label file to have the same stem.
+
+        Output
+        ------
+        samples:
+            A list of samples. Each sample is structured as `(image_file, label_file)`.
+        """
+        stem_to_image_file = {i.stem: i for i in images}
+        stem_to_label_file = {l.stem: l for l in labels}
+
+        samples: list[tuple[Path, Path]] = []
+
+        for stem, image_file in stem_to_image_file.items():
+            if stem not in stem_to_label_file:
+                self.logger.warning(f"Missing label file for {image_file}.")
+                continue
+            samples.append((image_file, stem_to_label_file[stem]))
+
+        return samples
+
+    def _filter_for_valid_labels(
+        self, samples: list[tuple[Path, Path]]
+    ) -> list[tuple[Path, Path]]:
+        """
+        Filter out samples with invalid labels.
+        """
+        expected_classes = set([f"{i}" for i in range(self.num_classes)])
+
+        filtered_samples: list[tuple[Path, Path]] = []
+        for sample in samples:
+            with open(sample[1], "r") as fp:
+                label = fp.read().strip()
+
+                if label not in expected_classes:
+                    self.logger.warning(f"Invalid label {label} in {sample[1]}.")
+                    continue
+
+                filtered_samples.append(sample)
+
+        return filtered_samples
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx: int) -> tuple[Tensor, Tensor]:
+        image_file, label_file = self.samples[idx]
+
+        # Load in the image and pre-process it
+        pil_image = Image.open(image_file)
+        image = pil_to_tensor(pil_image)
+        image = self.transform(image)
+
+        # Load in the label
+        one_hot_class = torch.zeros((self.num_classes,))
+        with open(label_file, "r") as fp:
+            class_id = int(fp.read().strip())
+        one_hot_class[class_id] = 1
+
+        return image, one_hot_class
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from uuid import uuid4
+
 import pytest
 import torch
 import numpy as np
+from PIL import Image
+from torch import Tensor
 
-from vit_tensorrt.data import Letterbox
+from vit_tensorrt.data import Letterbox, OptionalDivision, ViTDataset, ViTImageTransform
 
 
 class TestLetterbox:
@@ -39,3 +45,143 @@ class TestLetterbox:
         image = torch.randint(0, 255, (3, 320, 320))
         out_image: torch.Tensor = letterbox(image)
         assert out_image.shape == (3, letterbox.out_height, letterbox.out_width)
+
+
+class TestOptionalDivision:
+    @pytest.fixture
+    def division(self) -> OptionalDivision:
+        return OptionalDivision()
+
+    def test_init(self, division: OptionalDivision):
+        assert isinstance(division, OptionalDivision)
+
+    def test_divide(self, division: OptionalDivision):
+        """
+        Test that we divide the image when the max value is above 1.
+        """
+        image = torch.randint(0, 255, (3, 320, 320), dtype=torch.uint8)
+        result = division(image)
+
+        assert isinstance(result, Tensor)
+        assert 0 <= result.min()
+        assert result.max() <= 1
+
+    def test_skip_divide(self, division: OptionalDivision):
+        """
+        Test that we skip dividing the image when the max value is 1 or lower.
+        """
+        image = torch.ones((3, 320, 320), dtype=torch.uint8)
+        result = division(image)
+
+        assert isinstance(result, Tensor)
+        assert (result == 1).all()
+
+
+class TestViTImageTransform:
+    def test_init(self):
+        transform = ViTImageTransform(768, 768)
+        assert isinstance(transform, ViTImageTransform)
+
+    def test_transform(self):
+        """
+        Tests that the transforms are applied correctly.
+        """
+        height, width = 768, 768
+        transform = ViTImageTransform(height, width)
+
+        # Try to apply the transforms to an input image
+        image = torch.randint(0, 255, (3, 320, 320), dtype=torch.uint8)
+        result = transform(image)
+
+        assert isinstance(result, Tensor)
+        assert result.shape == (3, height, width)
+        assert result.dtype == torch.float32
+        assert result.max() < 100
+
+
+class TestViTDataset:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.temp_dir = TemporaryDirectory()
+        self.temp_path = Path(self.temp_dir.name)
+
+        self.images_path = self.temp_path / "images"
+        self.labels_path = self.temp_path / "labels"
+
+        self.images_path.mkdir(exist_ok=True, parents=True)
+        self.labels_path.mkdir(exist_ok=True, parents=True)
+
+        yield
+
+        self.temp_dir.cleanup()
+
+    def test_init_valid_dataset(self):
+        """
+        Test that we keep all valid samples when initialising the dataset.
+        """
+        # Make a single image file and label file that are valid
+        uuid = f"{uuid4()}"
+
+        im = Image.fromarray(np.zeros((320, 320, 3), dtype=np.uint8))
+        im.save(self.images_path / f"{uuid}.png")
+
+        with open(self.labels_path / f"{uuid}.txt", "w") as fp:
+            fp.write("1")
+
+        # Try to initialise the dataset
+        dataset = ViTDataset(self.images_path, self.labels_path, 2)
+        assert isinstance(dataset, ViTDataset)
+        assert len(dataset) == 1
+
+    def test_init_missing_label(self):
+        """
+        Test that we remove samples when we are missing the label.
+        """
+        # Make a single image file
+        uuid = f"{uuid4()}"
+
+        im = Image.fromarray(np.zeros((320, 320, 3), dtype=np.uint8))
+        im.save(self.images_path / f"{uuid}.png")
+
+        # Try to initialise the dataset
+        dataset = ViTDataset(self.images_path, self.labels_path, 2)
+        assert isinstance(dataset, ViTDataset)
+        assert len(dataset) == 0
+
+    def test_init_invalid_label_class(self):
+        """
+        Test that we remove samples with invalid label file contents.
+        """
+        # Make a single image file
+        uuid = f"{uuid4()}"
+
+        im = Image.fromarray(np.zeros((320, 320, 3), dtype=np.uint8))
+        im.save(self.images_path / f"{uuid}.png")
+
+        with open(self.labels_path / f"{uuid}.txt", "w") as fp:
+            fp.write("2")
+
+        # Try to initialise the dataset
+        dataset = ViTDataset(self.images_path, self.labels_path, 2)
+        assert isinstance(dataset, ViTDataset)
+        assert len(dataset) == 0
+
+    def test_test_get_item(self):
+        # Make a single image file and label file that are valid
+        uuid = f"{uuid4()}"
+
+        im = Image.fromarray(np.zeros((320, 320, 3), dtype=np.uint8))
+        im.save(self.images_path / f"{uuid}.png")
+
+        with open(self.labels_path / f"{uuid}.txt", "w") as fp:
+            fp.write("1")
+
+        # Try to get a sample from the dataset
+        num_classes = 2
+        dataset = ViTDataset(self.images_path, self.labels_path, num_classes, 768, 768)
+        image, label = dataset[0]
+
+        assert isinstance(image, Tensor)
+        assert image.shape == (3, dataset.image_height, dataset.image_width)
+        assert isinstance(label, Tensor)
+        assert label.shape == (num_classes,)
